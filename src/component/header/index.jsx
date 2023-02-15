24e49a4366e79def490fec4cdde9c94db80ec99e
Gankhulug <huluguu0202@gmail.com>
2023-02-15 14:44:03 +0800

    just for save my code

--- src/component/header/index.jsx
+++ src/component/header/index.jsx
@@ -3,14 +3,12 @@ import Bg from "./bgImage";
 import TopCategories from "./Categories";
 import Search from "./search";
 import VacationDestinations from "./vacationDestinations";
-import BookedSideBar from "../BookedSideBar";
 
 const Header = ({ destinations, isCategoryDetail, setIsCategoryDetail }) => {
   return (
     <div className="vh100">
       <Bg />
       <div className="space-between">
-        <BookedSideBar />
         <Search />
         <TopCategories
           destinations={destinations}
